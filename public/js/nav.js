45b579d9a8ba6158e34ae704fe6b9043941ef267
KevinFerracci <kevin.ferracci@gmail.com>
2021-01-26 05:45:29 +0100

    Homepage css

--- public/js/nav.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-  if (this.document.documentElement.scrollTop > window.innerHeight) {
-    this.document.querySelector("nav").style.background = "#4682B4";
-  }
-  else {
-    this.document.querySelector("nav").style.background = "none";
-  }
-} 
